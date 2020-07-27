61d7b4b20af4df5543c537ef5ed5fe30c9b92826
mambax7 <mambax7@gmail.com>
2020-07-26 21:57:25 -0400

    require

--- admin/header.php
+++ admin/header.php
@@ -25,7 +25,7 @@ declare(strict_types=1);
 use Xmf\Module\Admin;
 use XoopsModules\Wgtransifex\Helper;
 
-include dirname(__DIR__, 3) . '/include/cp_header.php';
+require dirname(__DIR__, 3) . '/include/cp_header.php';
 require_once \dirname(__DIR__) . '/include/common.php';
 $sysPathIcon16 = '../' . $GLOBALS['xoopsModule']->getInfo('sysicons16');
 $sysPathIcon32 = '../' . $GLOBALS['xoopsModule']->getInfo('sysicons32');
